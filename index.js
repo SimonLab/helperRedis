c24056e07a801f754949d19ddb6232cd8de837d7
Anita <a.nita@hotmail.co.uk>
2015-09-09 17:46:30 +0100

    redis data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Redis = require('redis');
 var Url = require('url');
+var Shortid = require('shortid');
 
 var url = "redis://127.0.0.1:6379/0";
 var RedisURL = Url.parse(url);
@@ -12,8 +13,8 @@ for (var i = 0; i<20; ++i) {
         date: "21-09-2015",
         artistName: "daVinci",
         artName: "Modernism",
-        tags: "fashion",
-        galleryName: "Holborn Gallery",
+        tags: "music",
+        galleryName: Shortid.generate(),
         free: true,
         endDate: "22-09-2015",
         permanent: false,
@@ -21,11 +22,24 @@ for (var i = 0; i<20; ++i) {
         hearts: 3,
         icon1: 2,
         icon2: 5,
-        location: 12344
+        location: 12344,
+        author: 'Jon'
     };
 
-    var pictures = JSON.stringify(['/assets/pictures/pic1.png', '/assets/pictures/pic2.png']);
+    var pictures = JSON.stringify(['/assets/pictures/pic1.jpg', '/assets/pictures/pic2.jpg', '/assets/pictures/pic3.jpg', '/assets/pictures/pic4.jpg']);
     review.pictures = pictures;
+
+
+    var extraReviews = [{
+        stars: 2,
+        hearts: 3,
+        icon1: 2,
+        icon2: 5,
+        author: "Anita",
+        pictures: ['/assets/pictures/pic1.jpg', '/assets/pictures/pic2.jpg', '/assets/pictures/pic3.jpg', '/assets/pictures/pic4.jpg']
+    }];
+    
+    review.extraReviews = JSON.stringify(extraReviews);
 
     Client.hmset('review:' + i, review);
 
@@ -34,4 +48,4 @@ for (var i = 0; i<20; ++i) {
 
 
 console.log('Data inserted');
-exit();
+
